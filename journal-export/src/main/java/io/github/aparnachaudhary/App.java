54dd941a44c418325f0f3370fde9d1e442dde35f
Aparna Chaudhary <aparna.chaudhary@gmail.com>
2016-02-15 13:17:41 +0100

    prototype to export journal data

---
+++ journal-export/src/main/java/io/github/aparnachaudhary/App.java
@@ -0,0 +1,23 @@
+package io.github.aparnachaudhary;
+
+import org.apache.activemq.artemis.cli.commands.tools.XmlDataExporter;
+
+/**
+ * Print JMS Journal in XML format
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        String jmsDataDir = "/Users/Aparna/dev/tools/wildfly-10.0.0.Final/standalone/data/activemq";
+        String bindingsDir = jmsDataDir + "/bindings";
+        String messageDir = jmsDataDir + "/journal";
+        String largeMessageDir = jmsDataDir + "/largemessages";
+
+        try {
+            new XmlDataExporter().process(System.out, bindingsDir, messageDir, messageDir, largeMessageDir);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
